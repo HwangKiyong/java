37686a31a3fc469e7cdfe82cb9ea8bc72244e095
황기용 <rldyd220@naver.com>
2023-01-05 10:35:51 +0900

    java

--- src/ch07/ex03/Main.java
+++ src/ch07/ex03/Main.java
@@ -1,7 +1,21 @@
 package ch07.ex03;
 
-public class Main {
+import ch07.ex03.dao.UserDao;
+import ch07.ex03.dao.UserDaoImpl;
+import ch07.ex02.presentation.Console;
+import ch07.ex03.presentation.UserIo;
+import ch07.ex03.sevice.UserService;
+import ch07.ex03.sevice.UserServiceImpl;
 
+public class Main {
+	public static void main(String[] args) {
+		UserDao userDao = new UserDaoImpl();
+		UserService userService = new UserServiceImpl(userDao);
+		UserIo userIo = new UserIo(userService);
+		
+		userIo.play();
+		Console.info("end.");
+	}
 }
 
 
