6d0c7fe5b9fa23c647dfd512e26b95cd5363bb07
황기용 <rldyd220@naver.com>
2023-01-11 15:23:38 +0900

    java

--- src/ch07/ex03/Main.java
+++ src/ch07/ex03/Main.java
@@ -1,8 +1,8 @@
 package ch07.ex03;
 
+import ch07.ex02.presentation.Console;
 import ch07.ex03.dao.UserDao;
 import ch07.ex03.dao.UserDaoImpl;
-import ch07.ex02.presentation.Console;
 import ch07.ex03.presentation.UserIo;
 import ch07.ex03.sevice.UserService;
 import ch07.ex03.sevice.UserServiceImpl;
--- src/ch09/ex04/Buffer.java
+++ src/ch09/ex04/Buffer.java
@@ -7,10 +7,28 @@ public class Buffer {
 		while(val == null) {
 			try {
 				wait(); //기다리는 것이다. sleep은 아니다.
-			}
+			} catch(InterruptedException e) {}			
 		}
+		
+		int val = this.val;
+		this.val = null;		
+		notifyAll();
+		
+		return val;
+	}
+	
+	public synchronized void add(Integer val) {
+		while(this.val != null) {
+			try {
+				wait(); //생상자는 값이 있으면 wait 한다.
+			} catch(InterruptedException e) {}		
+		}
+
+		this.val = val;
+		notifyAll();		
 	}
 }
  /*
-wait();	기다리는 것이다. sleep은 아니다.
+wait(); = 기다리는 것이다. sleep은 아니다. Object 클래스에 속한다.
+sleep(); thread 클래스에 속한다
 */
